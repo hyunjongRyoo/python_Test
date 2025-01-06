b828431f6abcbb8538812603684f0b7b13a39150
hyunjongRyoo <guswhd6656@naver.com>
2025-01-07 07:25:45 +0900

    01.06 python 강의 내용 커밋

---
+++ Test/pratice.py
@@ -0,0 +1,41 @@
+# #자료형
+# #숫자
+# print(5)
+# print(-10)
+# print(3.14)
+# print(1000)
+# print(5 + 3)
+# print(2 * 4)
+# print(3*(3+1))
+# #문자
+# print('풍선')
+# print("풍선")
+# print("z"* 9)
+# print(5 > 10)
+# print(5 < 10)
+# #boolean
+# print(True)
+# print(False)
+# print(not True)
+# print(not False)
+# print(not (5>10))
+# #변수
+# animal ="개"
+# name ="연탄이"
+# age = 4 
+# hobby = "산책"
+# is_adult = age >=3 
+# print("우리집 "+ animal +"의 이름은" + name + "예요")
+# hobby = "공놀이"
+# # print( name + "는" + str(age) + "살이며" +hobby+"을 아주 좋아합니다")
+# print( name , "는" , str(age) ,"살이며" ,hobby,"을 아주 좋아합니다")
+# print(name +"는 어른일까요?" + str(is_adult))
+# #주석
+# '''  
+# 전체적으로 주석처리 하고싶으면 작은 따옴표 3개 
+# '''
+# # 문장 전체 선택후 command / 주석처리
+
+# QUIZ CHATER 1
+station = "사당"
+print(station +"행 열차가 들어오고 있습니다")
