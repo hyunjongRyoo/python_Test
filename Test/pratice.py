e41442746352c9ffd8817d7719633318f08a1b43
hyunjongRyoo <guswhd6656@naver.com>
2025-01-09 22:21:00 +0900

    01.09 python 강의 커밋

--- Test/pratice.py
+++ Test/pratice.py
@@ -226,9 +226,178 @@ my_pw =my_pw[:my_pw.index(".")] #규칙 2
 password = my_pw[:3] + str(len(my_pw)) + str(my_pw.count("e")) + "!" 
 print("{0} 의 비밀번호는 {1} 입니다".format(pw, password))
 
+#리스트[]  - 배열
+subway1 =10
+subway2 =20
+subway3 =30
+
+subway=[10,20,30]
+print(subway)
+subway =["유재석", "조세호","박명수"]
+
+print(subway.index("조세호")) #몇번째 칸에 타고있는가(위치) [0,1,2]
+
+#하하가 다음 정류장에서 다음칸에 탑승(리스트에 추가)
+subway.append("하하")
+print(subway)
+
+#정형돈씨를 유재석/조세호 사이에 태워봄 넣을 위치를 적고 그 다음에 값을 적어야함
+subway.insert(1 , "정형돈" )
+print(subway)
+
+#지하철에 있는 사람을 한명씩 뒤에서 꺼냄
+print(subway.pop())
+print(subway)
+
+print(subway.pop())
+print(subway)
+
+print(subway.pop())
+print(subway)
+
+# #같은 이름의 사람이 몇 명 있는지 확인
+# subway.append("유재석") 
+# print(subway)
+# print(subway.count("유재석"))
+
+# #정렬  1,2,3,4,5
+# num_list =[5,2,4,3,1]
+# num_list.sort()
+# # print(num_list)
+
+# # #반대로 정렬
+# # num_list.reverse()
+# # print(num_list)
+
+# # #모두 지우기
+# # num_list.clear()
+# # print(num_list)
+
+# # #다양한 자료형 함께 사용
+# # num_list =[5,2,4,3,1]
+# # mix_list = ["조세호", 20, True]
+# # # print(mix_list)
+
+# # #리스트 확장
+# # num_list.extend(mix_list)
+# # print(num_list)
+
+# # #사전 키:밸류
+# # cabinet = {3:"유재석" ,  100:"김태호"}
+# # print(cabinet[3])
+# # print(cabinet[100])
+
+# # print(cabinet.get(3))
+# # print(cabinet.get(5))
+# # # print(cabinet[5]) 
+# # # 대괄호 실행시 값이 없으면 오류 종료 
+# # # get을 사용했을시 값이 없어도 다음 문장을 실행시킴
+
+# # print(cabinet.get(5))
+# # print(cabinet.get(5, "사용 가능"))
+# # print("hi")
+
+# # print(3 in cabinet) #in 을 사용하여 참 거짓 판별
+# # print(5 in cabinet)
+
+# cabinet = {"A-3":"유재석" , "B-100":"김태호"}
+
+# print(cabinet["A-3"])
+# print(cabinet["B-100"])
+
+# #추가
+# print(cabinet)
+# cabinet["A-3"] = "김종국"
+# cabinet["C-20"] = "조세호"
+# print(cabinet)
+
+# #삭제
+# del cabinet["A-3"]
+# print(cabinet)
+
+# #key 만 출력
+# print(cabinet.keys())
+
+# #value 만 출력
+# print(cabinet.values())
+
+# #둘다 출력 
+# print(cabinet.items())
+
+# #전부 삭제
+# cabinet.clear()
+# print(cabinet)
 
 
+# #튜플: 내용 변경이나 추가가 불가능 , 속도는 리스트 보다 빠르다
+# menu =("돈가스" ,  "치즈가스")
+# print(menu[0])
+# print(menu[1])
 
+# name= "김"
+# age= 20
+# hobby = "코딩"
+# print(name,age,hobby)
+
+# (name,age,hobby) =("김종국" , 20, "코딩")
+# print(name,hobby,age)
+
+# #집합(세트)
+# #중복 x , 순서 없음
+# my_set = {1,2,3,3,3}
+# print(my_set)
+
+# java ={"유재석" , "김태호", "양세형"}
+# python = set(["유재석","박명수"])
+
+# #교집합
+# print(java &python)
+# print(java.intersection(python))
+
+# #합집합 (둘중 하나라도 가능)
+# print(java | python)
+# print(java.union(python))
+
+# #차집합 (java 가능 python 불가능)
+# print(java - python)
+# print(java.difference(python))
+
+# #추가
+# python.add("김태호")
+# print(python)
+
+# java.remove("김태호")
+# print(java)
+
+# #자료 구조의 변경
+# menu ={"커피","주스","우유"}
+# print(menu, type(menu))
+
+# menu = list(menu)
+# print(menu, type(menu))
+
+# menu =tuple(menu)
+# print(menu, type(menu))
+
+# menu =set(menu)
+# print(menu, type(menu))
+
+#Quiz
+
+from random import *
+users = range(1,21) #1~20 범위 설정
+users = list(users)
+
+print(users)
+shuffle(users)
+print(users)
+
+winners = sample(users , 4)
+
+print(" -- 당첨자 발표 --")
+print("  치킨 당첨자 : {0}".format(winners[0]))
+print("  커피 당첨자 :{0}".format(winners[1:]))
+print(" -- 축하합니다 --")
 
 
 
